5fef737e95dbf2ab0b0e82de6a46b248237a6c73
Jaroslav Zhanel <zhanel@tech4bet.com>
2021-10-22 13:30:05 +0200

    new

--- index.js
+++ index.js
@@ -19,24 +19,58 @@ app.post('/posts/:id/comments', async (req, res) => {
     const { content } = req.body;
 
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content })
+    comments.push({ id: commentId, content, status: 'pending' })
 
     commentsByPostId[req.params.id] = comments;
 
-    await axios.post('http://localhost:4005/events', {
-        type: 'CommentCreated',
-        data: {
-            id: commentId,
-            content,
-            postId: req.params.id
-        }
-    });
+    try {
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentCreated',
+            data: {
+                id: commentId,
+                content,
+                postId: req.params.id,
+                status: 'pending'
+            }
+        });
+    } catch (e) {
+        console.log(e.message)
+    }
+
+
 
     res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Reicived event', req.body.type);
+
+    const {type, data} = req.body;
+    if(type === "CommentModerated") {
+        const { postId, id, status, content } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find((comment) => {
+            return comment.id === id;
+        });
+        comment.status = status;
+
+        try {
+            await axios.post('http://localhost:4005/events', {
+                type: 'CommentUpdated',
+                data: {
+                    id,
+                    status,
+                    postId,
+                    content
+                }
+            });
+        } catch (e) {
+            console.log(e.message)
+        }
+
+
+    }
 
     res.send({})
 });
